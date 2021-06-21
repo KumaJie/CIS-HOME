335fd181f140e66ccf053bbc3ab0112dae1098d9
Baiyusoup <1575727840@qq.com>
2021-06-21 10:49:51 +0800

    修改一些人性化设计

--- src/views/department/department.vue
+++ src/views/department/department.vue
@@ -1,153 +1,164 @@
 <template>
-	<div>
-		<div>
-			<el-form ref="form" :inline="true" :model="selectForm" label-width="100px">
-				<el-form-item label="输入部门名称">
-					<el-input v-model="selectForm.deptName"></el-input>
-				</el-form-item>
-			  
-				<el-form-item >
-					<el-button type="primary" size="small" @click="select">查询</el-button>
-				</el-form-item>
-				
-				<el-form-item >
-					<el-button type="primary" size="small" @click="">删除</el-button>
-				</el-form-item>
-			</el-form>
-		</div>
-		
-		<el-table :data="talbeData" border>
-			<el-table-column type="selection"> </el-table-column>
-			<el-table-column label="部门名称" prop="deptName"> </el-table-column>
-			<el-table-column label="详细信息" prop="deptRemark"> </el-table-column>
-			<el-table-column label="操作">
-				<template slot-scope="scope">
-					<el-button type="primary" size="small" @click="handleEdit(scope.row)">编辑</el-button>
-					<el-button type="danger" size="small" @click="deleteDept(scope.row)">删除</el-button>
-				</template>
-			</el-table-column>
-		</el-table>
-		
-		<el-dialog
-		  title="提示"
-		  :visible.sync="dialogVisible"
-		  width="30%">
-		  <span>
-			  <el-form ref="form" :model="editForm" label-width="80px">
-			    <el-form-item label="部门名称">
-			      <el-input v-model="editForm.deptName"></el-input>
-			    </el-form-item>
-			    
-			    <el-form-item label="详细描述">
-			      <el-input v-model="editForm.deptRemark"></el-input>
-			    </el-form-item>
-			  </el-form>
-		  </span>
-		  <span slot="footer" class="dialog-footer">
-		    <el-button @click="dialogVisible = false">取 消</el-button>
-		    <el-button type="primary" @click="edit">确 定</el-button>
-		  </span>
-		</el-dialog>
-	</div>
-	
+  <div>
+    <div>
+      <el-form
+        ref="form"
+        :inline="true"
+        :model="selectForm"
+        label-width="100px"
+      >
+        <el-form-item label="输入部门名称">
+          <el-input v-model="selectForm.deptName"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" size="small" @click="select"
+            >查询</el-button
+          >
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" size="small" @click="deleteDept"
+            >删除</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+
+    <el-table :data="talbeData" border>
+      <el-table-column type="selection"> </el-table-column>
+      <el-table-column label="部门名称" prop="deptName"> </el-table-column>
+      <el-table-column label="详细信息" prop="deptRemark"> </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button type="primary" size="small" @click="handleEdit(scope.row)"
+            >编辑</el-button
+          >
+          <el-button type="danger" size="small" @click="deleteDept(scope.row)"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <span>
+        <el-form ref="form" :model="editForm" label-width="80px">
+          <el-form-item label="部门名称">
+            <el-input v-model="editForm.deptName"></el-input>
+          </el-form-item>
+
+          <el-form-item label="详细描述">
+            <el-input v-model="editForm.deptRemark"></el-input>
+          </el-form-item>
+        </el-form>
+      </span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="edit">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-			
-			selectForm: {
-				deptName: '',
-			},
-			
-			dialogVisible: false,
-						
-			editForm: {
-				deptName: '',
-				deptRemark: '',
-			},
-			updateItemId: null,
-            talbeData: [],
-        };
+  data() {
+    return {
+      selectForm: {
+        deptName: ""
+      },
+
+      dialogVisible: false,
+
+      editForm: {
+        deptName: "",
+        deptRemark: ""
+      },
+      updateItemId: null,
+      talbeData: []
+    };
+  },
+  methods: {
+    select() {
+      this.$http({
+        url: "/getDept",
+        method: "GET",
+        params: {
+          deptName: this.selectForm.deptName
+        }
+      }).then(res => {
+        const data = res.data;
+        if ((data.status >= 200 && data.status < 300) || data.status === 304) {
+          this.talbeData = data.data;
+        } else {
+          this.$message({ type: "warning", message: data.message });
+        }
+      });
     },
-    methods: {
-		
-		select(){
-			this.$http({
-				url: "http://localhost:8080/getDept",
-				method: "GET",
-				params: {
-					deptName: this.selectForm.deptName,
-				}
-			}).then(res => {
-				const data = res.data;
-				if ((data.status >= 200 && data.status < 300) || data.status === 304) {
-					this.talbeData = data.data;
-				} else {
-					this.$message({ type: 'warning', message: data.message })
-				}
-			})
-		},
-		
-		handleEdit(item) {
-			this.dialogVisible = true;
-			this.updateItemId = item.deptId;
-		},
-        
-		edit() {
-			if (this.updateItemId === null || this.editForm.deptName === "" || this.editForm.deptRemark === "") {
-				this.$message({type: 'warning', message: "修改字段不能为空!"});
-				return
-			}
-			this.$http({
-				url: "http://localhost:8080/updateDept",
-				method: "POST",
-				data: {
-					deptId: this.updateItemId,
-					deptName: this.editForm.deptName,
-					deptRemark: this.editForm.deptRemark,
-				}
-			}).then(res => {
-				const data = res.data;
-				this.dialogVisible = false;
-				this.$message({ type: 'warning', message: data.message });
-				this.select()
-			})
-        },
-			  
-		deleteDept(detailInfo) {
-			
-			this.$confirm('此操作将永久删除该部门信息, 是否继续?', '提示', {
-			          confirmButtonText: '确定',
-			          cancelButtonText: '取消',
-			          type: 'warning'
-			        }).then(() => {
-						this.$http({
-									  url: "http://localhost:8080/deleteDept",
-									  method: "POST",
-									  data: {
-										  ids: [detailInfo.deptId]
-									  }
-						}).then(res => {
-							const data = res.data;
-							this.$message({
-							  type: 'success',
-							  message: '删除成功!'
-							});
-							this.select()
-						})
-			        }).catch(() => {
-			          this.$message({
-			            type: 'info',
-			            message: '已取消!'
-			          });          
-			        });
-		}
+
+    handleEdit(item) {
+      this.dialogVisible = true;
+      this.updateItemId = item.deptId;
     },
+
+    edit() {
+      if (
+        this.updateItemId === null ||
+        this.editForm.deptName === "" ||
+        this.editForm.deptRemark === ""
+      ) {
+        this.$message({ type: "warning", message: "修改字段不能为空!" });
+        return;
+      }
+      this.$http({
+        url: "/updateDept",
+        method: "POST",
+        data: {
+          deptId: this.updateItemId,
+          deptName: this.editForm.deptName,
+          deptRemark: this.editForm.deptRemark
+        }
+      }).then(res => {
+        const data = res.data;
+        this.dialogVisible = false;
+        this.$message({ type: "warning", message: data.message });
+        this.select();
+      });
+    },
+
+    deleteDept(detailInfo) {
+      this.$confirm("此操作将永久删除该部门信息, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$http({
+            url: "/deleteDept",
+            method: "POST",
+            data: {
+              ids: [detailInfo.deptId]
+            }
+          }).then(res => {
+            const data = res.data;
+            this.$message({
+              type: "success",
+              message: "删除成功!"
+            });
+            this.select();
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消!"
+          });
+        });
+    }
+  }
 };
 </script>
 
 <style>
-	
 </style>
--- src/views/department/departmentAdd.vue
+++ src/views/department/departmentAdd.vue
@@ -1,45 +1,54 @@
 <template>
-	<el-form ref="form" :model="addDeptForm" label-width="80px">
-	  <el-form-item label="部门名称">
-	    <el-input v-model="addDeptForm.deptName"></el-input>
-	  </el-form-item>
-	  
-	  <el-form-item label="详细描述">
-	    <el-input v-model="addDeptForm.deptRemark"></el-input>
-	  </el-form-item>
-	  
-	  <el-form-item>
-	    <el-button type="primary" @click="addDept">立即添加</el-button>
-	  </el-form-item>
-	</el-form>
+  <el-form ref="deptAddForm" :model="addDeptForm" label-width="80px">
+    <el-form-item label="部门名称">
+      <el-input v-model="addDeptForm.deptName"></el-input>
+    </el-form-item>
+
+    <el-form-item label="详细描述">
+      <el-input v-model="addDeptForm.deptRemark"></el-input>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="primary" @click="addDept">立即添加</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
 export default {
-    data() {
-      return {
-        addDeptForm: {
-			deptName: '',
-			deptRemark: '',
-		}
+  data() {
+    return {
+      addDeptForm: {
+        deptName: "",
+        deptRemark: ""
       }
-    },
-    methods: {
-      addDept() {
-		  this.$http({
-			  url: "http://localhost:8080/addDept",
-			  method: "POST",
-			  data: {
-				  deptName: this.addDeptForm.deptName,
-				  deptRemark: this.addDeptForm.deptRemark,
-			  }
-		  }).then(res => {
+    };
+  },
+  methods: {
+    addDept() {
+      this.$http({
+        url: "/addDept",
+        method: "POST",
+        data: {
+          deptName: this.addDeptForm.deptName,
+          deptRemark: this.addDeptForm.deptRemark
+        }
+      }).then(res => {
 				const data = res.data;
-					this.$message({ type: 'warning', message: data.message })
-			})
-      }
-    }
+        if ((data.status >= 200 && data.status < 300) || data.status === 304) {
+					this.$message({ type: "success", message: data.message });
+        } else {
+					this.$message({ type: "warning", message: data.message });
+        }
+				this.resetForm();
+      });
+    },
+		resetForm() {
+			this.addDeptForm.deptName = "";
+			this.addDeptForm.deptRemark = "";
+		}
   }
+};
 </script>
 
 <style>
