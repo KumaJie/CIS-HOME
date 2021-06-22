b1300e79b2caa6b481eb9c08c2dc2d020a9d82b7
lala <1156326157@qq.com>
2021-06-22 09:17:02 +0800

    新增下载和上传

--- src/views/document/downLoad.vue
+++ src/views/document/downLoad.vue
@@ -1,132 +1,200 @@
 <template>
-    <div>
-        <el-form :inline="true" :model="formData">
-            <el-form-item label="标题">
-                <el-input v-model="formData.documentTitle"></el-input>
-            </el-form-item>    
-            <el-form-item>
-                <el-button type="primary">搜索</el-button>
-                <el-button type="danger">删除</el-button>
-            </el-form-item>
-        </el-form>
-        <el-table :data="talbeData" border >
-            <el-table-column type="selection"> </el-table-column>
-            <el-table-column label="标题" prop="documentTitle" align="center"> </el-table-column>
-            <el-table-column label="创建时间" prop="documentCreate" align="center">
-            </el-table-column>
-            <el-table-column label="创建人" prop="userId" align="center"> </el-table-column>
-            <el-table-column label="描述" prop="docementRemark" align="center"> </el-table-column>
-            <el-table-column label="操作" width="90px" align="center">
-                <template slot-scope="scope">
-                    <el-button type="danger" size="small" @click="edit(scope.row)"
-                        >编辑</el-button
-                    >
-                </template>
-            </el-table-column>
-            <el-table-column label="下载" width="90px" align="center">
-                <template slot-scope="scope">
-                    <el-button
-                        type="primary"
-                        size="small"
-                        @click="downLoad(scope.row)"
-                        >下载</el-button
-                    >
-                </template>
-            </el-table-column>
-        </el-table>
-        <el-dialog :visible.sync="dialogFormVisible">
-            <el-form :model="formData">
-                <el-form-item label="文档标题">
-                        <el-input v-model="formData.documentTitle" style="width:300px;"></el-input>
-                </el-form-item>
-                <el-form-item label="文档描述">
-                    <el-input type="textarea" rows="10" v-model="formData.documentRemark"></el-input>
-                </el-form-item>
-                <el-form-item label="文档">
-                    <br/><el-upload
-                        class="upload-demo"
-                        action="https://jsonplaceholder.typicode.com/posts/"
-                        :on-preview="handlePreview"
-                        :on-remove="handleRemove"
-                        :before-remove="beforeRemove"
-                        multiple
-                        :limit="1"
-                        :on-exceed="handleExceed"
-                        :file-list="fileList"
-                        accept="text"
-                        >
-                        <el-button size="small" type="primary">选择文件</el-button>
-                        <span v-if="!flag" slot="tip" class="el-upload__tip">未选择任何文件</span>
-                    </el-upload>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary">上传</el-button>
-                    <el-button @click="resetForm('formData')">重置</el-button>
-                </el-form-item>
-            </el-form>
-            <div slot="footer">
-                <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="change">确 定</el-button>
-            </div>
-        </el-dialog>
-    </div>
+  <div>
+    <el-form :inline="true" :model="formData">
+      <el-form-item label="标题">
+        <el-input v-model="title"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="search">搜索</el-button>
+        <el-button type="danger" @click="deleteSelection">删除</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+      :data="talbeData"
+      border
+      class="table"
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column type="selection"> </el-table-column>
+      <el-table-column label="标题" prop="documentTitle" align="center">
+      </el-table-column>
+      <el-table-column label="创建时间" prop="documentCreate" align="center">
+      </el-table-column>
+      <el-table-column label="创建人" prop="userId" align="center">
+      </el-table-column>
+      <el-table-column label="描述" prop="documentRemark" align="center">
+      </el-table-column>
+      <el-table-column label="操作" width="90px" align="center">
+        <template slot-scope="scope">
+          <el-button type="danger" size="small" @click="edit(scope.row)"
+            >编辑</el-button
+          >
+        </template>
+      </el-table-column>
+      <el-table-column label="下载" width="90px" align="center">
+        <template slot-scope="scope">
+          <el-button type="primary" size="small" @click="downLoad(scope.row)"
+            >下载</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="50"
+      @current-change="changePage"
+    >
+    </el-pagination>
+    <el-dialog :visible.sync="dialogFormVisible" @close="dialogclose">
+      <el-form :model="formData">
+        <el-form-item label="文档标题">
+          <el-input
+            v-model="formData.documentTitle"
+            style="width: 300px"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="文档描述">
+          <el-input
+            type="textarea"
+            rows="10"
+            v-model="formData.documentRemark"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="文档">
+          <br /><el-upload
+            class="upload-demo"
+            action="http://localhost:8080/updateDocs"
+            :data="formData"
+            ref="upLoad"
+            :auto-upload="false"
+            :on-change="fileChange"
+            :limit="1"
+            :file-list="fileList"
+          >
+            <el-button size="small" type="primary">选择文件</el-button>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer">
+        <el-button @click="dialogclose">取 消</el-button>
+        <el-button type="primary" @click="change">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            formData:{
-                documentTitle:"文档哈",
-                documentName:"",
-                documentRemark:"这是一个文件"
-            },
-            talbeData: [
-                {
-                    documentTitle:"文件哈",
-                    documentCreate:"2019年4月6日",
-                    userId:"拉阿拉",
-                    docementRemark:"这是一个文件",
-                }
-            ],
-            flag:false,
-            fileList: [],
-            dialogFormVisible: false,
-        };
+  data() {
+    return {
+      title: "",
+      formData: {},
+      talbeData: [],
+      flag: false,
+      fileList: [],
+      multipleSelection: [], //存放已选择的数据
+      dialogFormVisible: false,
+    };
+  },
+  methods: {
+    // 点击修改按钮触发
+    edit(detailInfo) {
+      this.dialogFormVisible = true;
+      this.formData = { ...detailInfo };
+      delete this.formData.documentCreate;
+      this.fileList.push({ name: this.formData.documentName, url: "" });
     },
-    methods: {
-        edit(detailInfo) {
-            this.dialogFormVisible = true;
-            console.log(detailInfo);
-        },
-        change(){
-            this.dialogFormVisible = false;
-        },
-        downLoad(detailInfo) {
-            console.log(detailInfo);
-        },
-
-        resetForm(formName){
-            this.formData={
-                documentTitle:"",
-                documentName:"",
-                documentRemark:""
-            }
-        },
-        handleRemove(file, fileList) {
-            console.log(file, fileList);
-        },
-        handlePreview(file) {
-            console.log(file);
-        },
-        handleExceed(files, fileList) {
-            this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
-        },
-        beforeRemove(file, fileList) {
-            return this.$confirm(`确定移除 ${ file.name }？`);
+    // 改变上传文件
+    fileChange(file, fileList) {
+      this.fileList.pop();
+      this.flag = true;
+      this.fileList = fileList;
+      this.getDoc(1);
+    },
+    // 保存修改
+    change() {
+      if (this.flag) {
+        this.$refs.upLoad.submit();
+        setTimeout(()=>{
+          this.getDoc(1);
+        },1000)
+      } else {
+        this.$http.post("/updateDocsText", this.formData).then((e) => {
+          this.getDoc(1);
+        });
+      }
+      this.dialogFormVisible = false;
+    },
+    //关闭对话框
+    dialogclose() {
+      this.fileList = [];
+      this.formData = {};
+      this.dialogFormVisible = false;
+      this.getDoc(1);
+    },
+    // 下载
+    downLoad(detailInfo) {
+      this.$http.post("/downLoad", detailInfo).then((res) => {
+        window.location.href = res.data.message;
+      });
+    },
+    // 获取文档
+    getDoc(page = 1) {
+      this.$http
+        .get("getDocs", {
+          params: {
+            page,
+          },
+        })
+        .then((res) => {
+          this.talbeData = res.data.data;
+        });
+    },
+    changePage(page) {
+      this.getDoc(page);
+    },
+    // 删除
+    deleteSelection() {
+      if (this.multipleSelection.length == 0)
+        return this.$message.error("请选择要删除的文档");
+      let ids = [];
+      for (var i = 0; i < this.multipleSelection.length; i++) {
+        ids.push(this.multipleSelection[i].documentId);
+      }
+      this.$http.post("/deleteDocs", { ids }).then((e) => {
+        if (e.data.message == "删除成功") {
+          this.$message.success("删除成功");
+        } else {
+          this.$message.error("删除失败");
         }
+        this.getDoc(1);
+      });
     },
+    //搜索
+    search() {
+      this.$http
+        .get("getDocs", {
+          params: {
+            documentTitle: this.title,
+          },
+        })
+        .then((res) => {
+          this.talbeData = res.data.data;
+        });
+    },
+    // 点击多选框发生的变化
+    handleSelectionChange(val) {
+      this.multipleSelection = val; //给定义的数组赋值
+    },
+  },
+  mounted() {
+    this.getDoc(1);
+  },
 };
 </script>
 <style>
+.table {
+  margin-bottom: 20px;
+}
 </style>
